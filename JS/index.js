4ceedcbe546285debf91fb1df7cff129639d56b0
= <ismoilbek20050421@gmail.com>
2022-06-29 13:33:15 +0500

    Music-Player-JS

---
+++ JS/index.js
@@ -0,0 +1,122 @@
+window.addEventListener("scroll",()=>{
+  let header = document.querySelector(".header");
+  header.classList.toggle("active",window.scrollY > 0);
+});
+
+// MUSIC PLAYER TO THE CODES START
+
+let MusicImage = document.querySelector(".section__box_music_play_img");
+let musicPlay = document.querySelector(".section__box_music_play_icons .fa-play");
+let musicPause = document.querySelector(".section__box_music_play_icons .fa-pause");
+let next = document.querySelector(".fa-forward");
+let prev = document.querySelector(".fa-backward");
+let title = document.querySelector(".section__box_music_play_title");
+let audio = document.getElementById("audio");
+let navText = document.querySelector(".nav__text");
+let progress = document.querySelector(".section__box_music_play_border_box");
+let progressContainer = document.querySelector(".section__box_music_play_border");
+let faPause = document.getElementById("fa-pause");
+let faPlay = document.querySelector(".fa-circle-play");
+let index = 0;
+
+
+let MusicPlayerName = ["Burn It down","MBAND","User not found"];
+
+function MusicPlayerFunction(song){
+  MusicImage.src = `../IMG/MUSIC-IMAGES/${song}.jpg`;
+  audio.src = `../MUSIC/${song}.mp3`;
+  title.innerHTML = song;
+}
+MusicPlayerFunction(MusicPlayerName[index]);
+
+// MUSIC PLAY BUTTON 
+
+musicPlay.addEventListener("click",justPlay);
+
+function justPlay(){
+  audio.play();
+  musicPlay.style.display = "none";
+  musicPause.style.display = "block";
+
+}
+
+// MUSIC PAUSE TO THE CODES START 
+
+musicPause.addEventListener("click",justPause);
+
+
+function justPause(){
+  audio.pause();
+  musicPlay.style.display = "block";
+  musicPause.style.display = "none";
+}
+
+// MUSIC NEXT TO THE CODES START 
+
+next.addEventListener("click",justNextPlay);
+
+function justNextPlay(){
+  index++;
+  if(index > MusicPlayerName.length - 1){
+    index = 0;
+  }
+  MusicPlayerFunction(MusicPlayerName[index]);
+  audio.play();
+  justPlay();
+  navText.innerHTML = `0${1+index}/03`;
+}
+
+// MUSIC PREV TO THE CODES START 
+
+prev.addEventListener("click",justPrevPlay);
+
+function justPrevPlay(){
+  index--;
+  if(index < 0){
+    index = MusicPlayerName.length - 1
+  }
+  MusicPlayerFunction(MusicPlayerName[index]);
+  audio.play();
+  justPlay();
+  navText.innerHTML = `0${1+index}/03`;
+}
+
+// SETPROGRESS TO THE CODES START
+
+audio.addEventListener("timeupdate",Progress);
+
+function Progress(e){
+  let {duration, currentTime} = e.srcElement;
+  let progressTime = (currentTime / duration) * 100;
+  progress.style.width = `${progressTime}%`;
+}
+
+progressContainer.addEventListener("click",setProgress);
+
+function setProgress(e){
+  let width = this.clientWidth;
+  let clickX = e.offsetX;
+  let duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// FAPLAY TO THE CODES START 
+faPlay.addEventListener("click",faPlayerMusicPlayer);
+
+function faPlayerMusicPlayer(){
+  faPlay.style.display = "none";
+  faPause.style.display = "block";
+  MusicPlayerFunction(MusicPlayerName[index]);
+  audio.play();
+}
+
+// FAPAUSE TO THE CODES START 
+
+faPause.addEventListener("click",faPauserMusicPlayer);
+
+function faPauserMusicPlayer(){
+  faPlay.style.display = "block";
+  faPause.style.display = "none";
+  MusicPlayerFunction(MusicPlayerName[index]);
+  audio.pause();
+}
